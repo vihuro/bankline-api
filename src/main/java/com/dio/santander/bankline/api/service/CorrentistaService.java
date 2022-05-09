c7c6c2317c2ea37b11f019d164bb580431ddb537
vitor <vitorhugorodrigues93@gmail.com>
2022-05-09 09:57:41 -0300

    versão final do nosso projeto

--- src/main/java/com/dio/santander/bankline/api/service/CorrentistaService.java
+++ src/main/java/com/dio/santander/bankline/api/service/CorrentistaService.java
@@ -14,6 +14,7 @@ import com.dio.santander.bankline.api.repository.CorrentistaRepository;
 public class CorrentistaService {
 	@Autowired
 	private CorrentistaRepository repository;
+	
 	public void save (NovoCorrentista novoCorrentista) {
 		Correntista correntista = new Correntista();
 		correntista.setCpf(novoCorrentista.getCpf());
